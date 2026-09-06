31db70b1fc75e8de7dc684f2a108e49872355a68
branigak-mawgak <branigak@gmail.com>
2026-09-06 08:07:59 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='amalan-rc80-6-v1';
+const CACHE='amalan-rc80-7-v1';
 const ASSETS=['./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
 
 self.addEventListener('install',event=>{
